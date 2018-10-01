ccf64253ad473f136c26366ddf183f2e35e90a32
temachkin11 <temachkin11@gmail.com>
2018-10-01 11:20:47 +0300

    eazy

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
-module.exports = function getZerosCount(number) {
-  // your implementation
-}
+module.exports = function getZerosCount(n) {
+
+  var zeroCounts = 0;
+  
+  while(n > 0) {
+  
+  n=Math.floor(n / 5);
+  
+  zeroCounts += n
+  
+  }
+  
+  return zeroCounts; 
+  
+  }
